5c6d54fe08b903f025a4143b6c7974eb597dced4
David Nelson <david.nelson1@uqconnect.edu.au>
2025-04-08 13:20:18 +1000

    Set event schema.

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,1 +1,14 @@
-export class Event {}
+import { User } from 'src/users/entities/user.entity';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+
+@Entity('events')
+export class Event {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'datetime' })
+  createdAt: Date;
+
+  @ManyToOne(() => User, (user) => user.events)
+  user: User;
+}
