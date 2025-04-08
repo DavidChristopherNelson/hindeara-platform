278c5eb85882d98510798462191e1926adc16964
David Nelson <david.nelson1@uqconnect.edu.au>
2025-04-08 15:01:41 +1000

    Modify update-app.dto.ts.

--- src/apps/dto/update-app.dto.ts
+++ src/apps/dto/update-app.dto.ts
@@ -1,6 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAppDto } from './create-app.dto';
 
-export class UpdateAppDto extends PartialType(
-  OmitType(CreateAppDto, ['id', 'appInstances'] as const),
-) {}
+export class UpdateAppDto extends PartialType(CreateAppDto) {}
