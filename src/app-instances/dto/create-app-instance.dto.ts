2f8a2f1e82a7d064b9aa4d1c780d6e70a09bf758
David Nelson <david.nelson1@uqconnect.edu.au>
2025-04-09 11:31:15 +1000

    Write app instance create dto.

--- src/app-instances/dto/create-app-instance.dto.ts
+++ src/app-instances/dto/create-app-instance.dto.ts
@@ -1,1 +1,19 @@
-export class CreateAppInstanceDto {}
+import { IsEnum, IsNotEmpty, IsNumber } from 'class-validator';
+import { AppStatus } from '../entities/app-instance.entity';
+
+export class CreateAppInstanceDto {
+  @IsEnum(AppStatus, {
+    message: `Status must be one of the following values: ${Object.values(AppStatus).join(', ')}`,
+  })
+  status: AppStatus;
+
+  // The ID of the associated HindearaEvent entity.
+  @IsNotEmpty()
+  @IsNumber()
+  hindearaEventId: number;
+
+  // The ID of the associated App entity.
+  @IsNotEmpty()
+  @IsNumber()
+  appId: number;
+}
