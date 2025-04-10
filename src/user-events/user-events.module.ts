092e7a3fafedfb84c780423fc172bfd1bbf3e999
David Nelson <david.nelson1@uqconnect.edu.au>
2025-04-10 12:02:35 +1000

    Rewrite user-event module.

--- src/user-events/user-events.module.ts
+++ src/user-events/user-events.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserEventsService } from './user-events.service';
 import { UserEventsController } from './user-events.controller';
+import { UsersModule } from 'src/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEvent } from './entities/user-event.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEvent]), UsersModule],
   controllers: [UserEventsController],
   providers: [UserEventsService],
 })
