d310dde2652c27f4ee725d79619901ffe51a189a
David Nelson <david.nelson1@uqconnect.edu.au>
2025-04-10 12:42:18 +1000

    Remove hindeara-event and app-instance.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { EventsModule } from './events/events.module';
-import { AppInstancesModule } from './app-instances/app-instances.module';
-import { RecordingsModule } from './recordings/recordings.module';
 import { AppsModule } from './apps/apps.module';
-import { HindearaEventsModule } from './hindeara-events/hindeara-events.module';
 import { UserEventsModule } from './user-events/user-events.module';
 import { AppEventsModule } from './app-events/app-events.module';
 
 @Module({
-  imports: [UsersModule, EventsModule, AppInstancesModule, RecordingsModule, AppsModule, HindearaEventsModule, UserEventsModule, AppEventsModule],
+  imports: [UsersModule, AppsModule, UserEventsModule, AppEventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
