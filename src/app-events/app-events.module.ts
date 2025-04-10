4259e3f87f09b7768fb4e943dabb171f049e44c0
David Nelson <david.nelson1@uqconnect.edu.au>
2025-04-10 12:27:25 +1000

    Rewrite app-event module.

--- src/app-events/app-events.module.ts
+++ src/app-events/app-events.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppEventsService } from './app-events.service';
 import { AppEventsController } from './app-events.controller';
+import { UsersModule } from 'src/users/users.module';
+import { AppEvent } from './entities/app-event.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AppEvent]), UsersModule],
   controllers: [AppEventsController],
   providers: [AppEventsService],
 })
