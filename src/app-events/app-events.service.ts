80a2b8dfc092fdcfe2b0d4c7eb2912270e5bd127
David Nelson <david.nelson1@uqconnect.edu.au>
2025-04-10 12:26:38 +1000

    Rewrite app-event service.

--- src/app-events/app-events.service.ts
+++ src/app-events/app-events.service.ts
@@ -1,26 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAppEventDto } from './dto/create-app-event.dto';
-import { UpdateAppEventDto } from './dto/update-app-event.dto';
+import { AppEvent } from './entities/app-event.entity';
+import { Repository } from 'typeorm';
+import { UsersService } from 'src/users/users.service';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class AppEventsService {
-  create(createAppEventDto: CreateAppEventDto) {
-    return 'This action adds a new appEvent';
+  constructor(
+    @InjectRepository(AppEvent)
+    private appEventRepository: Repository<AppEvent>,
+    private readonly usersService: UsersService,
+  ) {}
+
+  async create(createAppEventDto: CreateAppEventDto): Promise<AppEvent> {
+    const user = await this.usersService.findOne(createAppEventDto.userId);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    const appEvent = this.appEventRepository.create({
+      ...createAppEventDto,
+      user,
+    });
+    return this.appEventRepository.save(appEvent);
   }
 
-  findAll() {
-    return `This action returns all appEvents`;
+  async findAll(): Promise<AppEvent[]> {
+    return this.appEventRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} appEvent`;
-  }
-
-  update(id: number, updateAppEventDto: UpdateAppEventDto) {
-    return `This action updates a #${id} appEvent`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} appEvent`;
+  async findOne(id: number): Promise<AppEvent | null> {
+    return this.appEventRepository.findOne({ where: { id } });
   }
 }
