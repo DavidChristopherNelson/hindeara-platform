7d543bb885a6b978158d61bea4d9f0ab1d029a6a
David Nelson <david.nelson1@uqconnect.edu.au>
2025-04-10 11:51:40 +1000

    Rewrite user-event controller.

--- src/user-events/user-events.controller.ts
+++ src/user-events/user-events.controller.ts
@@ -1,34 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { UserEventsService } from './user-events.service';
 import { CreateUserEventDto } from './dto/create-user-event.dto';
-import { UpdateUserEventDto } from './dto/update-user-event.dto';
+import { UserEvent } from './entities/user-event.entity';
 
 @Controller('user-events')
 export class UserEventsController {
   constructor(private readonly userEventsService: UserEventsService) {}
 
   @Post()
-  create(@Body() createUserEventDto: CreateUserEventDto) {
+  async create(
+    @Body() createUserEventDto: CreateUserEventDto,
+  ): Promise<UserEvent> {
     return this.userEventsService.create(createUserEventDto);
   }
 
   @Get()
-  findAll() {
+  async findAll(): Promise<UserEvent[]> {
     return this.userEventsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userEventsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserEventDto: UpdateUserEventDto) {
-    return this.userEventsService.update(+id, updateUserEventDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userEventsService.remove(+id);
+  async findOne(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<UserEvent | null> {
+    return this.userEventsService.findOne(id);
   }
 }
