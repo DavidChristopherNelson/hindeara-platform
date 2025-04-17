968928663aded576da579b96426b3b3dc2f198f3
David Nelson <david.nelson1@uqconnect.edu.au>
2025-04-17 12:31:45 +1000

    Find currentApp.

--- src/apps/apps.service.ts
+++ src/apps/apps.service.ts
@@ -5,12 +5,14 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { App } from './entities/app.entity';
 import { AppEvent } from 'src/app-events/entities/app-event.entity';
+import { AppEventsService } from 'src/app-events/app-events.service';
 
 @Injectable()
 export class AppsService {
   constructor(
     @InjectRepository(App)
     private readonly appRepository: Repository<App>,
+    private readonly appEventsService: AppEventsService,
   ) {}
 
   async create(createAppDto: CreateAppDto): Promise<App> {
@@ -38,7 +40,39 @@ export class AppsService {
   async findCurrentApp(user: User): Promise<App> {
     const pastAppEvents: AppEvent[] =
       await this.appEventsService.findAllByUser(user);
-      // TODO: const currentApp = Do logic and either return an element of pastAppEvents or call this.chooseNewApp
-      
+    // TODO: const currentApp = Do logic and either return an element of pastAppEvents or call this.chooseNewApp
+
+    const fiveMinutesAgo = Date.now() - 5 * 60_000;
+    const listOfCompletedApps: App[] = [];
+
+    for (const appEvent of pastAppEvents) {
+      if (appEvent.createdAt.getTime() < fiveMinutesAgo) {
+        return this.chooseNewApp();
+      }
+      if (appEvent.is_complete) {
+        listOfCompletedApps.push(appEvent.app);
+        continue;
+      }
+      if (listOfCompletedApps.includes(appEvent.app)) {
+        return this.chooseNewApp();
+      }
+      return appEvent.app;
+    }
+
+    console.warn(
+      'Warning: findCurrentApp() did not return from the loop. This should not have happened. Returning fallback new app.',
+    );
+    return this.chooseNewApp();
+  }
+
+  async chooseNewApp(): Promise<App> {
+    const alfaApp = await this.findOne(1);
+    if (!alfaApp) {
+      return this.create({
+        http_path: 'https://alfa.com',
+        is_active: true,
+      });
+    }
+    return alfaApp;
   }
 }
