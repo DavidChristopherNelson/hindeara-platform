5505a50b8549b16d66b04a8a83492ff826b796af
David Nelson <david.nelson1@uqconnect.edu.au>
2025-04-17 15:24:31 +1000

    Remove exposed create methods for app-events and user-events controllers.

--- src/app-events/app-events.controller.ts
+++ src/app-events/app-events.controller.ts
@@ -1,25 +1,10 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  ParseIntPipe,
-} from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { AppEventsService } from './app-events.service';
-import { CreateAppEventDto } from './dto/create-app-event.dto';
 import { AppEvent } from './entities/app-event.entity';
 
 @Controller('app-events')
 export class AppEventsController {
   constructor(private readonly appEventsService: AppEventsService) {}
-
-  @Post()
-  async create(
-    @Body() createAppEventDto: CreateAppEventDto,
-  ): Promise<AppEvent> {
-    return this.appEventsService.create(createAppEventDto);
-  }
 
   @Get()
   async findAll(): Promise<AppEvent[]> {
--- src/user-events/user-events.controller.ts
+++ src/user-events/user-events.controller.ts
@@ -1,25 +1,10 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  ParseIntPipe,
-} from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { UserEventsService } from './user-events.service';
-import { CreateUserEventDto } from './dto/create-user-event.dto';
 import { UserEvent } from './entities/user-event.entity';
 
 @Controller('user-events')
 export class UserEventsController {
   constructor(private readonly userEventsService: UserEventsService) {}
-
-  @Post()
-  async create(
-    @Body() createUserEventDto: CreateUserEventDto,
-  ): Promise<UserEvent> {
-    return this.userEventsService.create(createUserEventDto);
-  }
 
   @Get()
   async findAll(): Promise<UserEvent[]> {
