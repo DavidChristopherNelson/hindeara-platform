1d1a0e66d1241d218acd71a3dee4f9a66f3750d4
David Nelson <david.nelson1@uqconnect.edu.au>
2025-04-20 11:55:48 +1000

    Modify alfa-app/interface methods.

--- src/apps/alfa-app/interface/dto/update-interface.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateInterfaceDto } from './create-interface.dto';
-
-export class UpdateInterfaceDto extends PartialType(CreateInterfaceDto) {}
--- src/apps/alfa-app/interface/interface.service.ts
+++ src/apps/alfa-app/interface/interface.service.ts
@@ -1,26 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { CreateInterfaceDto } from './dto/create-interface.dto';
-import { UpdateInterfaceDto } from './dto/update-interface.dto';
+import { User } from 'src/hindeara-platform/users/entities/user.entity';
+import { CreateAppEventDto } from 'src/hindeara-platform/app-events/dto/create-app-event.dto';
 
 @Injectable()
 export class InterfaceService {
-  create(createInterfaceDto: CreateInterfaceDto) {
+  async run(user: User): Promise<CreateAppEventDto> {
     return 'This action adds a new interface';
-  }
-
-  findAll() {
-    return `This action returns all interface`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} interface`;
-  }
-
-  update(id: number, updateInterfaceDto: UpdateInterfaceDto) {
-    return `This action updates a #${id} interface`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} interface`;
   }
 }
