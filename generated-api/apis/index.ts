8dc7ea72d41481d26aa1f1521d31bc1cc7edd16c
David Nelson <david.nelson1@uqconnect.edu.au>
2025-05-21 12:33:07 +0530

    feature/autogenerated Open API.

---
+++ generated-api/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AppEventsApi';
+export * from './AppsApi';
+export * from './MiniLessonsApi';
+export * from './PlatformsApi';
+export * from './UserEventsApi';
+export * from './UsersApi';
---
+++ generated-api/models/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CreateAppDto';
+export * from './ProcessUserInputDto';
+export * from './ProcessUserInputResponseDto';
